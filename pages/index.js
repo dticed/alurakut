3db92833e755893546727063d7521246ebb09567
dticed <lucaspierrealencar@gmail.com>
2021-07-12 17:23:15 -0300

    teste

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
-import styled from 'styled-components'
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  const githubUser = 'dticed'
+
+  return (
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <Box>
+          <img src={`https://github.com/${githubUser}.png`} />
+        </Box>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box>
+          Bem vindo
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <Box>
+          Pessoas da comunidade
+        </Box>
+        <Box>
+          Comunidades
+        </Box>
+      </div>
+    </MainGrid>
+  )
 }
